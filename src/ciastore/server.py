73a807ee1a893ab347bd87649112d8aa7fd6163f
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2023-01-06 17:39:34 -0600

    Add initial python module structure

---
+++ src/ciastore/server.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Server - Caught In the Act Server
+
+"Caught In the Act Server"
+
+
+__title__ = 'Caught In the Act Server'
+__author__ = 'CSHS Members'
+__version__ = '0.0.0'
+
+
+
+
+
+def run() -> None:
+    """Synchronous Entry Point"""
+
+
+
+
+
+if __name__ == '__main__':
+    print(f'{__title__}\nProgrammed by {__author__}.\n')
+    run()
---
+++ tests/test_server.py
@@ -0,0 +1,2 @@
+import pytest
+from ciastore import server
